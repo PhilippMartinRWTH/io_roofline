7cd62df55d8ed5c7dbc014107170d292def0673f
Philipp Mark Martin <pm.martin@itc.rwth-aachen.de>
2025-09-19 20:08:09 +0200

    added mdtest data

---
+++ README.md
@@ -0,0 +1,8 @@
+# Data Repository for "Optimising HPC Filesystem Choices with Bandwidth-Centric I/O Rooflines"
+
+## Data
+The data/ directory contains all measurement data relevant for the submission and a plotting script to create different plots.
+Each entry in the sorted.csv file corresponds to the averages of 10 mdtest runs.
+
+## SLURM Scripts
+The slurm_scripts directory contains sample scripts of how the data was generated.
---
+++ data/plot_bw.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+data = pd.read_csv('sorted.csv')
+
+#bandwidth [byte/s]
+data['bw_write'] = (data['bytes'] * data['write_mean_rate'])
+data['bw_read'] = (data['bytes'] * data['read_mean_rate']) 
+
+data['bw_write_sd'] = (data['bytes'] * data['write_sd_rate'])
+data['bw_read_sd'] = (data['bytes'] * data['read_sd_rate']) 
+
+#densities [byte/IOPS]
+data['density_write'] = data['bytes']
+data['density_read'] = data['bytes']
+
+#intensities [IOPS/byte]
+data['intensity_write'] = 1 / data['bytes']
+data['intensity_read'] = 1 / data['bytes']
+
+colors = ['#003a7d', '#d83034', '#ff9d3a', '#4ecb8d', '#ff73b6', 'black']
+markers = ['x','+','3','4','.',',']
+
+format = 'pgf'
+
+def plot(name, output, plots):
+    ax = None
+    count = 0
+
+    xlabel = 'Data Density [Byte/IOP]'
+    ylabel = 'Bandwidth [Byte/s]'
+
+    for pl in plots:
+        mydata = data.copy()
+        operation = 'write'
+        label = ''
+        for key in pl:
+            if key == 'operation':
+                operation = pl['operation']
+            elif key == 'label':
+                label = pl['label']
+            else:
+                mydata = mydata[(mydata[key] == pl[key])]
+
+        if format == 'pgf':
+            plt.rcParams.update({
+                "font.family": "serif",  # use serif/main font for text elements
+                "text.usetex": True,     # use inline math for ticks
+                "pgf.rcfonts": False,     # don't setup fonts from rc parameters
+            })
+
+        def mean_of_sd(x):
+            res = 0
+            for e in x:
+                res += e*e
+            res /= x.size
+            res = math.sqrt(res)
+            return res
+
+        #mydata = mydata.groupby('bytes').aggregate({'density_write':'mean', 'read_mean_rate':'mean', 'read_sd_rate':mean_of_sd, 'bw_read':'mean', 'bw_write':'mean', 'density_read':'mean'})
+        mydata = mydata.groupby('bytes').aggregate({
+            'density_write':'mean', 
+            'intensity_write':'mean', 
+            'bw_write':'mean', 
+            'bw_write_sd':mean_of_sd,
+            'write_mean_rate':'mean', 
+            'write_sd_rate':mean_of_sd, 
+            'density_read':'mean', 
+            'intensity_read':'mean', 
+            'bw_read':'mean', 
+            'bw_read_sd':mean_of_sd,
+            'read_mean_rate':'mean', 
+            'read_sd_rate':mean_of_sd, 
+            'stat_mean_rate':'mean',
+            'stat_sd_rate':mean_of_sd,
+            'removal_mean_rate':'mean',
+            'removal_sd_rate':mean_of_sd,
+            })
+
+        if label == '':
+            label = '{}'.format(pl['fs'])
+
+        xax = 'density_'+operation
+        yax = 'bw_'+operation
+        yerr = 'bw_'+operation+'_sd'
+
+        if operation in ['removal', 'stat']:
+            xax = 'intensity_write'
+            yax = operation+'_mean_rate'
+            yerr = operation+'_sd_rate'
+            xlabel = 'Metadata Intensity [IOP/Byte]'
+            ylabel = 'Metadata Rate [IOP/s]'
+
+        print("{} with {} and {}".format(operation, xax, yax))
+
+        if not ax:
+            ax = mydata.plot(x=xax, y=yax, yerr=yerr, kind='scatter', title=name, legend=True, label=label, color=colors[count], marker=markers[count])
+        else:
+            mydata.plot(x=xax, y=yax, yerr=yerr, kind='scatter', ax=ax, label=label, color=colors[count], marker=markers[count])
+
+        count = count+1
+
+
+    #ax.plot(foam_density, foam_bw, marker='x', linestyle='', color='red')
+    #ax.plot(foam_density, foam_bw_lustre, marker='x', linestyle='', color='blue')
+
+    plt.xscale('log',base=2)
+    plt.yscale('log',base=2)
+    #plt.ylim(bottom=1)
+    plt.legend(loc=4)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    plt.savefig(output+'.'+format)
+    plt.close()
+
+#MD-BW Comparison
+
+#Hypothesis 1: stat/unlink
+plot('Pure Metadata Operations','hyp1',[
+    {'cluster':'claix18','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'stat', 'label':'stat'},
+    {'cluster':'claix18','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'removal', 'label':'unlink'}
+    ])
+
+#Hypothesis 2: read/write
+plot('Bandwidth Operations','hyp2',[
+    {'cluster':'claix23','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'read', 'label':'read'},
+    {'cluster':'claix23','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'write'}
+    ])
+
+#Hypothesis 3a: Filesystems
+plot('Filesystem Comparison CLUSTER-2','hyp3a',[
+    {'cluster':'claix23','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'local'},
+    {'cluster':'claix23','fs':'beeond','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'ad-hoc'},
+    {'cluster':'claix23','fs':'lustre','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'parallel'}
+    ])
+
+plot('Filesystem Comparison CLUSTER-1','hyp3a1',[
+    {'cluster':'claix18','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'local'},
+    {'cluster':'claix18','fs':'beeond','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'ad-hoc'},
+    {'cluster':'claix18','fs':'lustre','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'parallel'}
+    ])
+
+#Hypothesis 3b: Node Counts
+plot('Node Count Comparison','hyp3b',[
+    {'cluster':'claix23','fs':'beeond','n_tasks':4,'n_nodes':4,'n_files':1000,'operation':'write', 'label':'4 Nodes'},
+    {'cluster':'claix23','fs':'beeond','n_tasks':4,'n_nodes':2,'n_files':1000,'operation':'write', 'label':'2 Nodes'},
+    {'cluster':'claix23','fs':'beeond','n_tasks':4,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'1 Node'},
+    ])
+
+#Hypothesis 3c: Task Counts
+plot('Task Count Comparison','hyp3c',[
+    {'cluster':'claix23','fs':'tmp','n_tasks':50,'n_nodes':1,'operation':'write', 'label':'50 Tasks'},
+    {'cluster':'claix23','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'1 Task'},
+    ])
+
+#Hypothesis 4: Caching
+plot('Caching Comparison', 'hyp4',[
+    {'cluster':'claix18','fs':'tmp','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'Cached'},
+    {'cluster':'claix18','fs':'tmp_odirect','n_tasks':1,'n_nodes':1,'n_files':1000,'operation':'write', 'label':'Uncached'},
+    ])
+
+#Hypothesis 5: OpenFOAM
+plot('OpenFOAM Motorbike Rooflines', 'hyp5', [
+    {'cluster':'claix23','fs':'lustre','n_tasks':96, 'n_nodes':1, 'operation':'write', 'label':'Parallel'},
+    {'cluster':'claix23','fs':'beeond','n_tasks':96, 'n_nodes':1, 'operation':'write', 'label':'Ad-Hoc'},
+    {'cluster':'claix23','fs':'tmp','n_tasks':96, 'n_nodes':1, 'operation':'write', 'label':'Local'}
+    ])
