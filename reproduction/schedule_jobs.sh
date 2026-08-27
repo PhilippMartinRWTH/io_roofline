f4afc793cd97475d15df7af0963a25cd71c8c6ad
Philipp Mark Martin <pm.martin@itc.rwth-aachen.de>
2026-08-28 00:35:57 +0200

    moved from dummy cat to sbatch

--- reproduction/schedule_jobs.sh
+++ reproduction/schedule_jobs.sh
@@ -50,9 +50,9 @@ fi
 OUTPUT_PATH=${PWD}/outputs
 
 for tasks (1 4 8 16 32 48 96); do
-    cat <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" -v r4="${tasks}" -v t4="__TASK_NUM__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); gsub(t4,r4); print }' roofline.slurm)
+    sbatch <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" -v r4="${tasks}" -v t4="__TASK_NUM__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); gsub(t4,r4); print }' roofline.slurm)
 done
 
-cat <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); print }' openfoam.slurm)
-cat <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); print }' cifar.slurm)
-cat <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" -v r4="${IMAGENET_PATH}" -v t4="__IMAGENET_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); gsub(t4,r4); print }' imagenet.slurm)
+sbatch <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); print }' openfoam.slurm)
+sbatch <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); print }' cifar.slurm)
+sbatch <(awk -v r1="${FS_PATH}" -v t1="__FS_PATH__" -v r2="${FS_NAME}" -v t2="__FS_NAME__" -v r3="${OUTPUT_PATH}" -v t3="__OUTPUT_PATH__" -v r4="${IMAGENET_PATH}" -v t4="__IMAGENET_PATH__" '{ gsub(t1,r1); gsub(t2,r2); gsub(t3,r3); gsub(t4,r4); print }' imagenet.slurm)
